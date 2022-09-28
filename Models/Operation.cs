af69e2e914f7f463173786cf4ad7130a6ea143ba
IHFox <61856245+IHFox@users.noreply.github.com>
2022-09-28 00:28:10 -0300

    Ajuste de organização para arquitetura MVC

---
+++ Models/Operation.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculadora.Models
+{
+    /// <summary>
+    /// Modelo para uma operação a ser incluída no banco de dados
+    /// </summary>
+    internal class Operation
+    {
+        public string FullOperation { get; set; } = string.Empty;
+    }
+}
