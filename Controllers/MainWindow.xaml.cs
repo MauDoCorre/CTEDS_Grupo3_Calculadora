6f40bc30094d2cc1fe808a368255cf69209b3228
IHFox <61856245+IHFox@users.noreply.github.com>
2022-09-28 02:05:40 -0300

    Adicionada lógica para operações de limpar e inverter sinal

--- Controllers/MainWindow.xaml.cs
+++ Controllers/MainWindow.xaml.cs
@@ -76,5 +76,53 @@ namespace Calculadora
             }
 
         }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+
+            switch (button.Content.ToString())
+            {
+                case "CE":
+                    txtDisplay.Text = "0";
+                    break;
+
+                case "C":
+                    string1 = "";
+                    string2 = "";
+                    operation = "";
+                    txtDisplay.Text = "0";
+                    txtOngoing.Text = "0";
+                    break;
+
+                case "DEL":
+                    txtDisplay.Text = txtDisplay.Text.Substring(0, txtDisplay.Text.Length - 1);
+                    break;
+
+            }
+        }
+
+        private void btnOther_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+
+            switch (button.Content.ToString())
+            {
+                case "+-":
+                    if(txtDisplay.Text.Substring(0, 1) != "-" && txtDisplay.Text != "0")
+                    {
+                        txtDisplay.Text = "-" + txtDisplay.Text;
+                    }
+                    else if (txtDisplay.Text != "0")
+                    {
+                        txtDisplay.Text = txtDisplay.Text.Substring(1);
+                    }
+                    break;
+
+                case ",":
+                    // Fazer implementação com números fracionários - necessário parse com Floats
+                    break;
+            }
+        }
     }
 }
