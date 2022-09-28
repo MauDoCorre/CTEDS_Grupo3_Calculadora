05900e75080b34672141f0e7c03dd292b5f563dc
IHFox <61856245+IHFox@users.noreply.github.com>
2022-09-28 00:51:27 -0300

    Adicionada interação com botões numéricos

--- Controllers/MainWindow.xaml.cs
+++ Controllers/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Calculadora
 {
@@ -20,9 +7,70 @@ namespace Calculadora
     /// </summary>
     public partial class MainWindow : Window
     {
+        long number = 0;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            number = (number * 10);
+            txtDisplay.Text = number.ToString();
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            number = (number * 10) + 1;
+            txtDisplay.Text = number.ToString();
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            number = (number * 10) + 2;
+            txtDisplay.Text = number.ToString();
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            number = (number * 10) + 3;
+            txtDisplay.Text = number.ToString();
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            number = (number * 10) + 4;
+            txtDisplay.Text = number.ToString();
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            number = (number * 10) + 5;
+            txtDisplay.Text = number.ToString();
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            number = (number * 10) + 6;
+            txtDisplay.Text = number.ToString();
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            number = (number * 10) + 7;
+            txtDisplay.Text = number.ToString();
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            number = (number * 10) + 8;
+            txtDisplay.Text = number.ToString();
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            number = (number * 10) + 9;
+            txtDisplay.Text = number.ToString();
         }
     }
 }
